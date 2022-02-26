b97367c695b799502c0534a74d1b3038d7267bdd
xinchaojava@163.com <xinchaojava@163.com>
2022-02-26 14:27:39 +0800

    测试

---
+++ xin/xinchao/xinchaoshop/src/main/java/com/xinchao/xinchaoshop/XinchaoshopApplication.java
@@ -0,0 +1,13 @@
+package com.xinchao.xinchaoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinchaoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinchaoshopApplication.class, args);
+    }
+
+}
---
+++ xin/xinchao/xinchaoshop/src/test/java/com/xinchao/xinchaoshop/XinchaoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinchao.xinchaoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinchaoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
